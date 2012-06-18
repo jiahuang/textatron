0096817f1813b7309adef2f438386a7d17ffe1e1
jiahuang <jialiya.huang@students.olin.edu>
2012-06-17 20:13:38 -0700

    extension has basic functionality

--- app/texatron.py
+++ app/texatron.py
@@ -39,10 +39,44 @@ app.config.from_object(__name__)
 ########################################################################
 # Routes
 ########################################################################
+@app.route('/command', methods=["GET"])
+def command():
+	# given a web page url, gets all the commands that other people 
+	# have made off of it
+	return
+
+@app.route('/command/new', methods=["POST"])
+def newCommand():
+	request.form.get('')
+	return json_res({'success': 'Sweet, your command has been added. Try texting '+cmd+' to '+TEXATRON_NUMBER})
+
+@app.route('/requests', methods=["POST"])
+def requests():
+  print "REACHED: /request\n"
+  # this request should only be accessed through twilio
+  fromNumber = request.form.get('From', None)
+  msg = clean(request.form.get('Body', None).lower())
+  log('access', 'REQUEST: '+fromNumber+' '+msg)
+
+  currDate = datetime.datetime.utcnow()
+  user = db.users.find_one({'number': fromNumber})
+  req = {'time':currDate, 'message':msg}
+    
+  db.users.update({'number':fromNumber}, {'$push':{'requests':req}})
+  log('access', "REACHED: new request "+msg+" from "+fromNumber)
+  Commander(fromNumber, msg).start()
+
+  return json_res({'success': 'hit requests'})
 
 ########################################################################
 # Static routes
 ########################################################################
+@app.route('/', methods=['GET'])
+def main():
+  if "logged_in" in session and session['logged_in']:
+    return render_template('settings.html')
+  else:
+    return render_template('main.html')
 
 ########################################################################
 # Entry
